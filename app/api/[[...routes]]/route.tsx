90b924806892ea935e068c85080b664528d59eab
Maitra Khatri <maitrakhatri@gmail.com>
2024-09-14 14:01:28 +0530

    feat: Hello World Frame

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,81 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from "frog";
+import { devtools } from "frog/dev";
+import { handle } from "frog/next";
+import { serveStatic } from "frog/serve-static";
+
+const app = new Frog({
+  assetsPath: "/",
+  basePath: "/api",
+  title: "Hello World frame",
+});
+
+app.frame("/", (c) => {
+  const { inputText, status } = c;
+  const username = inputText;
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background:
+            status === "response"
+              ? "linear-gradient(to top, #432889, #17101F)"
+              : "linear-gradient(to right, #432889, #17101F)",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {status === "response"
+            ? `Hiee ${username ? username : "Anon"} !! Nice meeting you`
+            : "Hello World !!"}
+        </div>
+        <div
+          style={{
+            color: "white",
+            fontSize: 30,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {status === "response"
+            ? `I'm here to play with frames, feel free to join me on this adventure  :)`
+            : `Don't be shy, say hie`}
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="What should I call you ?" />,
+      <Button>{status === "response" ? "Say hi again :)" : "Say hi !"}</Button>,
+      status === "response" && <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
+
+devtools(app, { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
